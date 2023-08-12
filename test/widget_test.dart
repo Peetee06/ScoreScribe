bf5ff687f902bf577746ac65db6be5775116b2d1
Peter Trost <trost@intervall.io>
2023-08-12 17:31:08 +0200

    Update tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,23 +8,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:scorecard/main.dart';
+import 'package:scorecard/main.dart' as app;
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    app.main();
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
     await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    await tester.pump();
+    await tester.pump();
+    final gamesTab = find.byKey(const ValueKey('gamesTab'));
+    expect(gamesTab, findsOneWidget);
   });
 }
