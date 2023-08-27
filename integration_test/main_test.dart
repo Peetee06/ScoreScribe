57676004ab9a02637d3a321899ce5d1e2b083487
Peter Trost <p.trost93@gmail.com>
2023-08-27 13:46:43 +0200

    Fix import

--- integration_test/main_test.dart
+++ integration_test/main_test.dart
@@ -2,7 +2,7 @@ import 'package:emulators/emulators.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
-import 'package:ScoreScribe/main.dart' as app;
+import 'package:scorescribe/main.dart' as app;
 
 // import 'page_objects/tabs_screen.dart';
 
